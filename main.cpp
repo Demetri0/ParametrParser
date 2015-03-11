690e5b8fc4dbe156f9658405238c300b18c16b81
Demetri0 <d3i0@ya.ru>
2015-03-11 21:49:56 +0600

    Primitive parsing

--- main.cpp
+++ main.cpp
@@ -3,10 +3,20 @@
 
 int main(int argc, char** argv)
 {
-    ParametrParser p(argc, argv);
-    p.setShortOption("i", [](std::string str){ std::cout << str << std::endl; } );
-    p.exec();
-    std::cout << "The end!" << std::endl;
+    std::string inFile  = "Input.txt";
+    std::string outFile = "Output.txt";
+    char* pars[6] = {"-o", "Out.txt", "-x", "--input-file", "In.txt", "-h"};
+    ParametrParser p(6, pars);
+    p.setShortOption("o", [&](std::string str){ outFile = str; } );
+    p.setOption("i", "input-file", [&](std::string str){ inFile = str; } );
+    p.setShortOption("h", [](std::string){ std::cout << "[HELP]" << std::endl; } );
+    try{
+        p.exec();
+    }catch(ParametrParser::UnknownOption e){
+        std::cerr << "Unknown option: " << e.optionName << std::endl;
+    }
+    std::cout << "Input  file: " << inFile << std::endl;
+    std::cout << "Output file: " << outFile << std::endl;
     return 0;
 }
 
--- parametrparser.h
+++ parametrparser.h
@@ -2,26 +2,29 @@
 #define PARAMETRPARSER_H
 
 #include <string>
-#include <list>
+#include <vector>
 #include <map>
 #include <functional>
 
 using std::string;
 using std::map;
-using std::list;
+using std::vector;
 using std::function;
 
 class ParametrParser
 {
 private:
     typedef function<void(string)> func;
-
+    char optionLetter = '-';
 
     int _argc;
-    list<string> _argv;
+    vector<string> _argv;
     map<string, func> _shortCallback;
     map<string, func> _longCallback;
 
+    bool isOption(string);
+    bool isShortOption(string);
+    bool isLongOption(string);
 
 public:
     ParametrParser(int argc, char** argv);
@@ -29,6 +32,12 @@ public:
     void setShortOption(string key, func callback);
     void setLongOption(string key, func callback);
     bool exec();
+
+    class UnknownOption{
+    public:
+        string optionName;
+        UnknownOption(string opt){optionName=opt;}
+    };
 };
 
 #endif // PARAMETRPARSER_H
